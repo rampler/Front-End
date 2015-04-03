228be4366af591a1b312cb76459f6091f1421d16
rampler <rampler@gmail.com>
2015-04-03 22:48:20 +0200

    Added modal to add action

--- OpenLayersTemp/fe-map.js
+++ OpenLayersTemp/fe-map.js
@@ -32,8 +32,7 @@ var map = new ol.Map({
 });
 
 var select_interaction,
-    draw_interaction,
-    modify_interaction;
+    draw_interaction;
 
 var $interaction_type = $('[name="interaction_type"]');
 $interaction_type.on('click', function(e) {
@@ -52,58 +51,25 @@ function addModifyInteraction() {
         }
     });
     map.addInteraction(select_interaction);
-
-    var selected_features = select_interaction.getFeatures();
-    selected_features.on('add', function(event) {
-        var feature = event.element;
-        feature.on('change', saveData);
-        $(document).on('keyup', function(event) {
-            if (event.keyCode == 46) {
-                selected_features.forEach(function(selected_feature) {
-                    var selected_feature_id = selected_feature.getId();
-                    selected_features.remove(selected_feature);
-                    var vectorlayer_features = vector_layer.getSource().getFeatures();
-                    vectorlayer_features.forEach(function(source_feature) {
-                        var source_feature_id = source_feature.getId();
-                        if (source_feature_id === selected_feature_id) {
-                            vector_layer.getSource().removeFeature(source_feature);
-                            saveData();
-                        }
-                    });
-                });
-                $(document).off('keyup');
-            }
-        });
-    });
-    //modify_interaction = new ol.interaction.Modify({
-    //    features: selected_features,
-    //    deleteCondition: function(event) {
-    //        return ol.events.condition.shiftKeyOnly(event) &&
-    //            ol.events.condition.singleClick(event);
-    //    }
-    //});
-    //map.addInteraction(modify_interaction);
 }
 
 function addDrawInteraction() {
     map.removeInteraction(select_interaction);
-    map.removeInteraction(modify_interaction);
 
     draw_interaction = new ol.interaction.Draw({
         source: vector_layer.getSource(),
-        type: /** @type {ol.geom.GeometryType} */ ("LineString")
+        type: ("LineString")
     });
     map.addInteraction(draw_interaction);
     draw_interaction.on('drawend', function(event) {
-        var id = uid();
-        event.feature.setId(id);
-        saveData();
+        getJSONcoordinates();
+        spawnModal();
     });
 }
 
 addDrawInteraction();
 
-function saveData() {
+function getJSONcoordinates() {
     var data_type = 'GeoJSON', format = new ol.format[data_type](), data;
     try { data = format.writeFeatures(vector_layer.getSource().getFeatures()); }
     catch (e) {
@@ -112,26 +78,24 @@ function saveData() {
     }
 
     console.log(data);
+    return data;
 }
 
-$("#delete").click(function() {
+function clearDrawings() {
     vector_layer.getSource().clear();
     if (select_interaction) {
         select_interaction.getFeatures().clear();
-    }
-    $('#data').val('');
-});
-
-function uid(){
-    var id = 0;
-    return function() {
-        if (arguments[0] === 0) {
-            id = 0;
-        }
-        return id++;
     }
 }
 
 $('#osm-checkbox').change(function(){
     layers[0].setVisible($('#osm-checkbox').is(':checked'));
+});
+
+function spawnModal(){
+    $('#addModal').modal('show');
+}
+
+$('#addModal').on('hidden.bs.modal', function (e) {
+    clearDrawings();
 });
